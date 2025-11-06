5fbc3471a907e3f62deb699aaf99c4b9828c57bd
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-06 23:54:46 +0700

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom"
+import { Analytics } from "@vercel/analytics/next"
+
 import Layout from "./layout"
 import HCIAPractice from "./hcia-practice"
 import Sentifica from "./sentifica"
@@ -10,17 +12,20 @@ import Fukuro from "./fukuro"
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<h1 className={`font-bold`}><i className="ri-arrow-left-line font-normal"/>&ensp;Click on the project</h1>} />
-        <Route path="/hcia-practice" element={<HCIAPractice />} />
-        <Route path="/sentifica" element={<Sentifica />} />
-        <Route path="/pyfarm" element={<Pyfarm />} />
-        <Route path="/kaitosoft" element={<Kaitosoft />} />
-        <Route path="/cinnamon" element={<Cinnamon />} />
-        <Route path="/fukuro" element={<Fukuro />} />
-      </Route>
-    </Routes>
+    <>
+      <Analytics />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<h1 className={`font-bold`}><i className="ri-arrow-left-line font-normal"/>&ensp;Click on the project</h1>} />
+          <Route path="/hcia-practice" element={<HCIAPractice />} />
+          <Route path="/sentifica" element={<Sentifica />} />
+          <Route path="/pyfarm" element={<Pyfarm />} />
+          <Route path="/kaitosoft" element={<Kaitosoft />} />
+          <Route path="/cinnamon" element={<Cinnamon />} />
+          <Route path="/fukuro" element={<Fukuro />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
 
