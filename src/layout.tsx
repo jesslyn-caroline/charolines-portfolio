edc0ec6460578752a6564549f26f3a6711bee8ec
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-17 23:50:02 +0700

    Add animation on projectcard, change Link into window.location.href

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,4 +1,5 @@
-import { Link, Outlet, useLocation } from "react-router"
+import { Outlet, useLocation } from "react-router"
+import { motion } from "framer-motion"
 import SwitchModeBtn from "./components/buttons/switch_mode_btn"
 import ProjectsCard from "./components/cards/projects_card"
 
@@ -7,20 +8,24 @@ function Layout() {
     const location = useLocation();
 
     return (
-    <div className={`flex justify-center w-full h-full bg-background text-text transition overflow-y-visible scroll-bar-hidden`}>
-        <div className={`max-w-[1920px] min-h-screen w-screen flex flex-col overflow-y-visible scroll-bar-hidden`}>
+    <div className={`flex justify-center bg-background text-text transition`}>
+        <div className={`max-w-[1920px] w-screen min-h-screen flex flex-col`}>
             <div className={`min-h-16 h-16 w-full px-8 md:px-16 lg:px-24 flex justify-end items-center`}>
                 <SwitchModeBtn />
             </div>
             <div className={`px-8 md:px-16 lg:px-24 pb-36`}> 
                 <Outlet />
-                <div className={`my-8`}>
+                <motion.div 
+                    initial={{ opacity: 0, x: "100%" }} whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1.8 }}
+                    viewport={{ once: location.pathname === '/'? true : false }}
+                    className={`my-8`}>
                     <ProjectsCard />
-                </div>
-                <Link to="/" className={`${location.pathname === '/'? "hidden" : "block"} text-primary text-sm group flex flex-row items-center gap-x-2`}>
+                </motion.div>
+                <div onClick={() => window.location.href = "/"} className={`${location.pathname === '/'? "hidden" : ""} cursor-pointer flex items-center gap-x-2 group`}>
                     <i className="ri-arrow-left-line text-xl"/>
                     <span className={`w-fit text-md group-hover:underline`}>Back to Home</span>
-                </Link>
+                </div>
             </div>
         </div>
     </div>
