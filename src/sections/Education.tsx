8a778e915e9b5fbede42b0521441ffa2dd2bb3bc
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-15 10:52:25 +0700

    fix indentation

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -6,14 +6,11 @@ function Education () {
     <div className={`h-fit flex flex-col items-center gap-y-3 px-5 lg:px-20 py-24`}>
         <h1 className={`text-4xl text-[#7B3306] text-center`}>Education</h1>
         <div className={`mt-6 md:max-w-3xl w-full flex flex-col gap-y-4`}>
-            {
-                ...educations.map((education) => {
-                    return (
-                        <EducationCard {...education}/>
-                    )
-                })
-            }
-
+            { ...educations.map((education) => {
+                return (
+                    <EducationCard {...education}/>
+                )
+            }) }
         </div>
     </div>
 
