730e235105060e66f9a76b8727768e76c4fc3f02
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-15 13:11:10 +0700

    add carousel for preview

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router"
 import { projects, type ProjectCardProps } from "../data/projects"
 import { RiArrowLeftLine } from "@remixicon/react"
 import StarBorder3 from "../assets/StarBorder3"
+import PreviewCarousel from "../components/carousel/PreviewCarousel"
 
 function ProjectDetail() {
     const { path } = useParams()
@@ -14,7 +15,7 @@ function ProjectDetail() {
     }, [])
 
     return (
-    <div className={`max-w-480 min-h-screen w-screen h-fit flex flex-col gap-y-5 px-20 py-16`}>
+    <div className={`max-w-480 min-h-screen w-screen h-fit flex flex-col gap-y-5 px-5 md:px-20 py-16`}>
         <Link to = '/' className={`flex flex-row gap-x-3`}>
             <RiArrowLeftLine className={`text-[#7B3306B3]`}/>
             <span className={`text-[#7B3306B3]`}>Back</span>
@@ -24,7 +25,7 @@ function ProjectDetail() {
             <h1 className={`mt-5 text-3xl text-[#7B3306]`}>{ project?.name }</h1>
             <StarBorder3 />
         </div>
-        
+        { project && <PreviewCarousel dir={project.previewSrc} total={project.total} /> }
     </div>
 )}
 
