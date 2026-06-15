356b7ce3d6969e84fbdd708d4e519773011af960
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-15 10:51:47 +0700

    move main page to another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
-import { useLocation } from "react-router"
-import NavigationBar from "./components/navigations/NavigationBar"
-import About from "./sections/About"
-import Education from "./sections/Education"
-import Experience from "./sections/Experience"
-import Hero from "./sections/Hero"
-import Technology from "./sections/Technology"
-import { useEffect } from "react"
-import Projects from "./sections/Projects"
+import { Route, Routes } from "react-router"
+import MainPage from "./components/pages/MainPage"
+import ProjectDetail from "./components/pages/ProjectDetail"
 
 function App() {
-  const location = useLocation()
-
-  useEffect(() => {
-    document.getElementById(location.hash.slice(1))?.scrollIntoView({ 
-      behavior: 'smooth' 
-    })
-  }, [])
 
   return (
-    <div className={`w-full h-full flex justify-center pb-40`}>
-      <NavigationBar />
-
-      <div className={`max-w-480 min-h-screen w-screen h-fit flex flex-col justify-center`}>
-        <Hero />
-        <div id='about'><About /></div>
-        <div id='skills'><Technology /></div>
-        <div id='education'><Education /></div>
-        <div id='experience'><Experience /></div>
-        <div id='projects'><Projects /></div>
-      </div>
-    </div>
-    
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/projects/:path" element={<ProjectDetail />} />
+    </Routes>
 )}
 
 export default App
