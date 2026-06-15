7dc035a02c3cae14d471d82dbcd948063460071d
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-15 16:07:13 +0700

    add w-full

--- src/assets/StarBorder3.tsx
+++ src/assets/StarBorder3.tsx
@@ -1,6 +1,6 @@
 function StarBorder3 () {
     return (
-    <div className={`flex flex-row items-center gap-x-4`}>
+    <div className={`w-full flex flex-row items-center gap-x-4`}>
         <div className={`w-full h-0.5 bg-linear-to-r from-[#FBBF2499] to-[#FFFFFF00]`} />
         <img src='/star-4.svg' alt='star border' className={`w-6 h-6`} />
     </div>
