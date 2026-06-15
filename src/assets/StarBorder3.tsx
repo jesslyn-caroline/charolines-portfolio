01e427335314df5dcd4e442bbe251d086d5af23c
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-15 11:42:58 +0700

    new assets

---
+++ src/assets/StarBorder3.tsx
@@ -0,0 +1,9 @@
+function StarBorder3 () {
+    return (
+    <div className={`flex flex-row items-center gap-x-4`}>
+        <div className={`w-full h-0.5 bg-linear-to-r from-[#FBBF2499] to-[#FFFFFF00]`} />
+        <img src='/star-4.svg' alt='star border' className={`w-6 h-6`} />
+    </div>
+)}
+
+export default StarBorder3
