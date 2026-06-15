340dc203089ec1525d8b534a697fa901ad0a065d
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-15 22:25:25 +0700

    fix: add justify-between

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -7,14 +7,16 @@ function ProjectCard({ name, shortDesc, path,  year, tags }: ProjectCardProps) {
     <div className={`
         bg-linear-to-br from-[#FFFBEB] to-[#FFF1F2] border-2 border-[#FFD23080] 
         hover:border-[#FFD230] hover:rotate-1 transition duration-500 
-        rounded-lg p-5 flex flex-col gap-y-2
+        rounded-lg p-5 flex flex-col gap-y-2 justify-between
     `}>
-        <div className={`flex flex-row justify-between items-center`}>
-            <span className={`text-[#FE9A0099] text-sm`}>{ year }</span>
-            <Link to={`/projects/${path}`} target="_blank"><RiExternalLinkLine className={`text-[#E1710080] size-5`} /></Link>
+        <div className={`flex flex-col gap-y-2`}>
+            <div className={`flex flex-row justify-between items-center`}>
+                <span className={`text-[#FE9A0099] text-sm`}>{ year }</span>
+                <Link to={`/projects/${path}`} target="_blank"><RiExternalLinkLine className={`text-[#E1710080] size-5`} /></Link>
+            </div>
+            <h3 className={`text-[#7B3306] text-xl font-medium`}>{ name }</h3>
+            <p className={`text-[#7B3306B3] text-sm leading-6`}>{ shortDesc }</p>
         </div>
-        <h3 className={`text-[#7B3306] text-xl font-medium`}>{ name }</h3>
-        <p className={`text-[#7B3306B3] text-sm leading-6`}>{ shortDesc }</p>
         <div className={`mt-3 flex flex-row flex-wrap gap-2`}>
             { ...tags.map((tag) => {
                 return (
