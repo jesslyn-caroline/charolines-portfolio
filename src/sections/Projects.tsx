16047f6cf593ad28ab5e1100d6854167b1af9773
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-15 13:11:19 +0700

    add responsive

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -7,7 +7,7 @@ function Projects() {
     <div className={`w-full flex flex-col justify-start items-center gap-y-3 px-5 md:px-20 py-24`}>
         <h1 className={`text-4xl text-[#7B3306] text-center`}>Projects</h1>
         <StarBorder1 />
-        <div className={`mt-5 w-full grid grid-cols-3 gap-4`}>
+        <div className={`mt-5 w-full grid grid-cols-2 md:grid-cols-3 gap-4`}>
             { ...projects.map((project) => {
                 return <ProjectCard {...project}/>
             }) }
