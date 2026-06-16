e46025749e23b0d341ead7134fdb09332ed820ff
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-16 22:20:44 +0700

    add: vice lead position at MPC

--- src/data/experiences.tsx
+++ src/data/experiences.tsx
@@ -8,15 +8,27 @@ export interface ExperienceCardProps {
 
 export const experiences:ExperienceCardProps[] = [
     {
+        company: 'Mikroskil Programming Club',
+        position: 'Vice Lead',
+        startDate: 'Dec 2024',
+        endDate: 'Present',
+        responsibilities: [
+            'Prepares proposals and post-event reports for competitive programming competitions and club activities',
+            'Assists in the preparation and organization of internal competitive programming competitions',
+            'Assists junior members in solving competitive programming problems by providing guidance when needed'
+        ]
+    },
+    {
         company: 'PT. Pelita Controlmatic Utama',
         position: 'Accounting Payable',
         startDate: 'Aug 2023',
         endDate: 'Aug 2025',
         responsibilities: [
-            'Processed and verified invoices to ensure accurate and timely payments',
-            'Assisted in monitoring payment schedules and resolving billing discrepancies',
-            'Utilized Accurate software for financial data entry, reporting, and transaction management',
-            'Prepared monthly tax reporting and assisted the internal tax team with financial documentation',
+            'Gathered and verified supporting documents to ensure the accuracy and validity of vendor invoices before payment processing',
+            'Reviewed invoices and prepared payment schedules in accordance with company procedures',
+            'Recorded purchase payments and maintained accounts payable data using Accurate software',
+            'Submitted monthly PPh 21 and PPh 23 invoice documentation to the Tax Team for tax compliance and reporting',
+            'Assisted in resolving invoice discrepancies through coordination with vendors and internal departments'
         ]
     }
 ]
