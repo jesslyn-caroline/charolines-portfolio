70e145c52ba1cd6d738c0a8254719b46f2ca403a
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-16 21:22:52 +0700

    fix: reorder and change coding camp desc

--- src/data/education.tsx
+++ src/data/education.tsx
@@ -9,6 +9,13 @@ export interface EducationCardProps {
 
 export const educations:EducationCardProps[] = [
     {
+        institution: 'Dicoding Coding Camp by DBS Foundation',
+        degree: 'Cohort AI Engineer',
+        startDate: 'Feb 2026',
+        endDate: 'July 2026',
+        description: 'Focused on Machine Learning and Deep Learning using TensorFlow, with applications in Natural Language Processing (NLP), Computer Vision, and Time Series Forecasting. Collaborated on Kelolain, an AI-powered financial and business management platform for MSMEs as a capstone project.'
+    },
+    {
         institution: 'Universitas Mikroskil',
         degree: 'Bachelor of Computer Science',
         startDate: '2023',
@@ -18,12 +25,5 @@ export const educations:EducationCardProps[] = [
             'Academic Outstanding Student Even Semester of 2024-2025 Academic Year',
             'Academic Outstanding Student Odd Semester of 2025-2026 Academic Year'
         ]
-    },
-    {
-        institution: 'Dicoding Coding Camp by DBS Foundation',
-        degree: 'Cohort AI Engineer',
-        startDate: 'Feb 2026',
-        endDate: 'July 2026',
-        description: 'Learning about AI, machine learning, deep learning, and NLP. Building projects using AI technologies and tools.'
     }
 ]
