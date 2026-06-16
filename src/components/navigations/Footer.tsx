27a8a6a080cb105cc100c6eeee947a72f33c9d1c
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-16 20:47:32 +0700

    add footer

---
+++ src/components/navigations/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+    <div className={`w-full h-16 flex justify-center items-center bg-linear-to-b from-[#FFFBEB] to-[#FFF1F2] border-t-2 border-[#FFD2304D]`}>
+        <span className={`text-[#973C00CC] text-sm`}>&copy; 2026 Jesslyn Caroline</span>
+    </div>
+)}
+
+export default Footer
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,6 +7,7 @@ import Projects from "../sections/Projects"
 import Technology from "../sections/Technology"
 import NavigationBar from "../components/navigations/NavigationBar"
 import { useEffect } from "react"
+import Footer from "../components/navigations/Footer"
 
 function MainPage() {
     const location = useLocation()
@@ -17,10 +18,10 @@ function MainPage() {
     })}, [])
 
     return (
-    <div className={`w-full h-full flex justify-center pb-40`}>
+    <div className={`w-full h-full flex flex-col justify-center`}>
         <NavigationBar />
 
-        <div className={`max-w-480 min-h-screen w-screen h-fit flex flex-col justify-center`}>
+        <div className={`max-w-480 min-h-screen w-screen h-fit flex flex-col justify-center pb-30`}>
             <Hero />
             <div id='about'><About /></div>
             <div id='skills'><Technology /></div>
@@ -28,6 +29,8 @@ function MainPage() {
             <div id='experience'><Experience /></div>
             <div id='projects'><Projects /></div>
         </div>
+
+        <Footer />
     </div>
 )}
 
