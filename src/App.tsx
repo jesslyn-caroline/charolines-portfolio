65cc05da0d36442ba940dcb5f90ad706f04264f2
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-06-17 12:24:00 +0700

    update: add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router"
+import { Analytics } from "@vercel/analytics/next"
 import MainPage from "./pages/MainPage"
 import ProjectDetail from "./pages/ProjectDetail"
 
 function App() {
   return (
+  <>
+    <Analytics />
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/projects/:path" element={<ProjectDetail />} />
     </Routes>
+  </>  
 )}
 
 export default App
